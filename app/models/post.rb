7d13430c3f65c8f88243bd501e4ac166182ccaa9
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:46:18 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
-class Post < ActiveRecord::Base
 
-  validates :title, inclusion: { in: %w(Won't\ Believe Secret Top Guess)}
+class Post < ActiveRecord::Base
+  validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+
+
+  def is_clickbait?
+    #binding.pry
+    if /Won't Believe/.match(title) == nil
+      errors.add(:title, "There was not a match")
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe Post, type: :model do
   let(:non_clickbait) { Post.new(valid_attrs.merge(title: "True Facts")) }
 
   it "is valid" do
-    binding.pry
     expect(valid_post).to be_valid
   end
 
